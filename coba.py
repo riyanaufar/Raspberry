91af82a3910385ffa0bf7e06136a696ee7a992b5
Riyan Aufar <aufar.riyan@gmail.com>
2022-09-24 15:05:05 +0700

    commit

--- coba.py
+++ coba.py
@@ -1,16 +1,18 @@
 LED_PIN = 17
+
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BOARD)
+
+GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(LED_PIN, GPIO.OUT)
 
 try:
-while True:
-    GPIO.output(LED_PIN, GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(LED_PIN, GPIO.LOW)
-    time.sleep(1)
+    while True:
+        GPIO.output(LED_PIN, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(LED_PIN, GPIO.LOW)
+        time.sleep(1)
 except KeyboardInterrupt:
     GPIO.cleanup()
 
