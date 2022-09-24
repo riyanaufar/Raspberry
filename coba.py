41d6d1636b9e9676848a60501460c79f2e098652
Riyan Aufar <aufar.riyan@gmail.com>
2022-09-24 14:54:30 +0700

    commit

--- coba.py
+++ coba.py
@@ -5,7 +5,12 @@ GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(LED_PIN, GPIO.OUT)
 
-GPIO.output(LED_PIN, GPIO.HIGH)
-time.sleep(1)
+try:
+while True:
+    GPIO.output(LED_PIN, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(LED_PIN, GPIO.LOW)
+    time.sleep(1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
 
-GPIO.cleanup()
