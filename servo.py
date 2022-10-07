03b56418b3bdaf0e9456e601995305f17436791c
Riyan Aufar <aufar.riyan@gmail.com>
2022-10-07 20:04:18 +0700

    dsa

--- servo.py
+++ servo.py
@@ -1,18 +1,30 @@
-import RPi.GPIO as gp  
+import RPi.GPIO as GPIO
 import time
-from time import sleep  
 
- gp.setmode(gp.BOARD)  
- gp.setup(12,gp.OUT)  
- pwm=gp.PWM(12,50)  
- pwm.start(0)  
- for i in range(0,181):  
-   sig=(i/18)+2  
-   pwm.ChangeDutyCycle(sig)  
-   sleep(0.03)  
- for i in range(180,-1,-1):  
-   sig=(i/18)+2  
-   pwm.ChangeDutyCycle(sig)  
-   sleep(0.03)  
- pwm.stop()  
- gp.cleanup()
+servoPIN = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
+
+p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+p.start(2.5) # Initialization
+try:
+  while True:
+    p.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(12.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(2.5)
+    time.sleep(0.5)
+except KeyboardInterrupt:
+  p.stop()
+  GPIO.cleanup()
