bc87c9d32c095a3852eac62ce4bac180d8e66c3e
Riyan Aufar <aufar.riyan@gmail.com>
2022-10-07 19:56:00 +0700

    dsa

--- servo.py
+++ servo.py
@@ -1,28 +1,16 @@
-import RPi.GPIO as GPIO
-
-# duty cycle, calibrate if needed
-MIN_DUTY = 5
-MAX_DUTY = 10
-
-servo_signal_pin = 13
-
-def deg_to_duty(deg):
-    return (deg - 0) * (MAX_DUTY- MIN_DUTY) / 180 + MIN_DUTY
-
-if __name__ == "__main__":
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
-
-    GPIO.setup(servo_signal_pin, GPIO.OUT)
-    # set pwm signal to 50Hz
-    servo = GPIO.PWM(servo_signal_pin, 50)
-    servo.start(0)
-
-    # loop from 0 to 180
-    for deg in range(181):
-        duty_cycle = deg_to_duty(deg)    
-        servo.ChangeDutyCycle(duty_cycle)
-
-    # cleanup the gpio pins
-    GPIO.cleanup()
-    
+ import RPi.GPIO as gp  
+ from time import sleep  
+ gp.setmode(gp.BOARD)  
+ gp.setup(12,gp.OUT)  
+ pwm=gp.PWM(12,50)  
+ pwm.start(0)  
+ for i in range(0,181):  
+   sig=(i/18)+2  
+   pwm.ChangeDutyCycle(sig)  
+   sleep(0.03)  
+ for i in range(180,-1,-1):  
+   sig=(i/18)+2  
+   pwm.ChangeDutyCycle(sig)  
+   sleep(0.03)  
+ pwm.stop()  
+ gp.cleanup()
