01b5cd895f770b34ea6b4a459368d19e86e95587
Riyan Aufar <aufar.riyan@gmail.com>
2022-10-07 19:40:34 +0700

    cpmmit

--- coba.py
+++ coba.py
@@ -1,28 +1,18 @@
+LED_PIN = 4
+
 import RPi.GPIO as GPIO
+import time
 
-# duty cycle, calibrate if needed
-MIN_DUTY = 5
-MAX_DUTY = 10
+GPIO.setmode(GPIO.BCM)
 
-servo_signal_pin = 13
+GPIO.setup(LED_PIN, GPIO.OUT)
 
-def deg_to_duty(deg):
-    return (deg - 0) * (MAX_DUTY- MIN_DUTY) / 180 + MIN_DUTY
-
-if __name__ == "__main__":
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
-
-    GPIO.setup(servo_signal_pin, GPIO.OUT)
-    # set pwm signal to 50Hz
-    servo = GPIO.pwm(servo_signal_pin, 50)
-    servo.start(0)
-
-    # loop from 0 to 180
-    for deg in range(181):
-        duty_cycle = deg_to_duty(deg)    
-        servo.ChangeDutyCycle(duty_cycle)
-
-    # cleanup the gpio pins
+try:
+    while True:
+        GPIO.output(LED_PIN, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(LED_PIN, GPIO.LOW)
+        time.sleep(1)
+except KeyboardInterrupt:
     GPIO.cleanup()
-    
+
