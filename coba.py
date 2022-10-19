89126502da5df309a1b65f85529c3f259c463235
Riyan Aufar <aufar.riyan@gmail.com>
2022-10-19 12:27:37 +0700

    commit

--- coba.py
+++ coba.py
@@ -38,20 +38,20 @@ def make_interpolater(left_min, left_max, right_min, right_max):
     rightSpan = right_max - right_min
 
     #compute the scale factor between left and right values
-    scaleFactor = int(rightSpan) / int(leftSpan)
+    scaleFactor = range(int(rightSpan)) / range(int(leftSpan))
     
     #create interpolation func pre-calculated scaleFactor
     def interp_fn(value):
         return right_min + (value-left_min)*scaleFactor
 
-    return float(interp_fn)
+    return interp_fn
 
 #RANGES
 scaler_sensor = make_interpolater(0, 1023, 0, 300)
 scaler_servo = make_interpolater(0, 1023, -40, 40)
 
 #now convert to scaled values using map
-scaled_ldr1 = float(map(scaler_sensor, chan1.value)) #ldr left
+scaled_ldr1 = map(scaler_sensor, chan1.value) #ldr left
 scaled_ldr2 = map(scaler_sensor, chan2.value) #ldr right
 scaled_ldr3 = map(scaler_sensor, chan3.value) #ldr up
 scaled_ldr4 = map(scaler_sensor, chan4.value) #ldr down
